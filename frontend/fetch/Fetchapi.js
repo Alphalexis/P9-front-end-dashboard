e6311c97680dcd00b03d4bd852e8884cd0fe60f6
Alphalexis <alexis.leonarduzzi@gmail.com>
2023-02-26 17:24:16 +0100

    26/02/2023 17:24

--- frontend/fetch/Fetchapi.js
+++ frontend/fetch/Fetchapi.js
@@ -1,5 +1,8 @@
 import {mainData, activity, averageSessions, performance} from "../../app/dataservice"
+import USER_MAIN_DATA from "../../app/data";
+
 const url = "http://localhost:3000/user";
+let isMock = true;
 
 /**
  * retrieve data 
@@ -9,21 +12,32 @@ const url = "http://localhost:3000/user";
  */
 
 const getData = async (id, categorie) => {
-    let urlCall = categorie ? url + `/${id}/${categorie}/` : url+ `/${id}/`;
-    console.log (urlCall)
+    if (isMock == true){
+        //TODO ici je récupere les donnèes depuis le fichier
+        const data = USER_MAIN_DATA.find(element => element.id === id);
+        return data
+    }
+    else {
+        //TODO ici je récupere les données depuis un appel d'API  
+        let urlCall = categorie ? url + `/${id}/${categorie}/` : url+ `/${id}/`;
+        console.log (urlCall)
+    
+        const data = await fetch(urlCall);
+        console.log (data)
+    
+        const dataFetch = await data.json();
+        console.log(dataFetch)
+    }
 
-    const data = await fetch(urlCall);
-    console.log (data)
-
-    const dataFetch = await data.json();
-    console.log(dataFetch)
+const dataFetch = [];
     
     switch (categorie) {
         case "activity": return new activity(dataFetch.data);
         case "average-sessions": return new  averageSessions(dataFetch.data);
         case "performance": return new performance(dataFetch.data);
-
+        //case
         default: return new mainData(dataFetch.data);
+        //defaut: [];
     }}
 
 export default getData
--- frontend/pages/Dashboard.jsx
+++ frontend/pages/Dashboard.jsx
@@ -4,16 +4,28 @@ import Grapharea from '../components/Grapharea'
 import Graphbar from '../components/Graphbar'
 import Graphradar from '../components/Graphradar'
 import Graphradial from '../components/Graphradial'
+import getData from '../fetch/Fetchapi'
+import { useParams } from "react-router-dom";
 
 function Dashboard() {
+    let { id } = useParams();
 
+    console.log(id, "id");
+  
+    const data = getData(id,"activity")
+    console.log(data,"data");
+    //const item = data.find(element => element.id === id);
+//TODO récuperer l'id du user
+//TODO faire appel a getData pour recuperer les données en fonction de l'id du user et la catégorie
+//TODO passer les données en props aux differents composants
     return (
-        <div>
-            <Grapharea></Grapharea>
-            <Graphbar></Graphbar>
-            <Graphradar></Graphradar>
-            <Graphradial></Graphradial>
-        </div>
+        <h1>Alexis</h1>
+        // <div>
+        // {item.map((item) => (
+        //     <><Grapharea id={item.id} sessions={item.sessions} day={item.sessions.day} session={item.sessions.sessionLength}></Grapharea><Graphbar></Graphbar><Graphradar></Graphradar><Graphradial></Graphradial></>
+        //     ))}
+        // </div>
+        
     );
 }
 
